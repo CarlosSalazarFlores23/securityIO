cb0ec9e515cf4bebe527090a6d15548a641dec1a
CarlosSalazarFlores23 <salazarcaf@gmail.com>
2021-10-19 08:08:59 -0500

    Actualiza Titulo

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Security I/O',
+      title: 'Security I/O App',
       theme: ThemeData(
         // This is the theme of your application.
         //

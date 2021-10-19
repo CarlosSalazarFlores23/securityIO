35655d03c5776b7caa8cb4afff25e9e593b8561c
CarlosSalazarFlores23 <salazarcaf@gmail.com>
2021-10-19 07:57:48 -0500

    cambio titulo 'Security I/0'

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Security I/O',
       theme: ThemeData(
         // This is the theme of your application.
         //
